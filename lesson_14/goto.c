b2dae205c15f54c1491b64674b1287e42ee8afb9
George Diakos <97686078+georgethecoder0@users.noreply.github.com>
2022-01-22 16:37:12 +0200

    Create goto.c

---
+++ lesson_14/goto.c
@@ -0,0 +1,17 @@
+/* goto.c: Deixnei tin xrisi tis goto */
+
+#include <stdio.h>
+
+main()
+{
+    int i=0;
+
+label: // To onoma tis etiketas
+    printf("\ni=%d",i);
+    
+    if (i<5) 
+    {
+       i++;
+       goto label;
+    }
+}
